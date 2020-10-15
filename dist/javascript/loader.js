cfc07a7c08a003bb25d6fe3de5340914910a8065
kousiclattala <kousiclattala112@gmail.com>
2020-10-15 15:31:05 +0530

    Made Few changes and Added Blog Page

--- dist/javascript/loader.js
+++ dist/javascript/loader.js
@@ -1,12 +1,10 @@
 const loader = document.querySelector(".loader");
 const main = document.querySelector(".container");
 
-
 function init() {
   setTimeout(() => {
     loader.style.opacity = 0;
     loader.style.display = "none";
-    
 
     main.style.display = "block";
     setTimeout(() => (main.style.opacity = 1), 50);
